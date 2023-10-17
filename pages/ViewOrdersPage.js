59654278ed7a294135fc1e14e666c9168228dcfc
Sean Bryant <smbryant1730@gmail.com>
2023-10-17 03:31:58 -0400

    can now view open orders, next is to view details

---
+++ pages/ViewOrdersPage.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import OpenOrderCard from '../components/OpenOrderCard';
+import getAllOrders from '../api/OrderEndpoints';
+
+const ViewOrdersPage = () => {
+  const [openOrders, setOpenOrders] = useState([]);
+
+  useEffect(() => {
+    // Fetch open orders from your API
+    const fetchOpenOrders = async () => {
+      try {
+        const ordersData = await getAllOrders(); // Update with your API call
+        const openOrdersData = ordersData.filter((order) => order.orderStatus === 'Open');
+        console.log('Open Orders Data:', openOrdersData);
+
+        setOpenOrders(openOrdersData);
+      } catch (error) {
+        console.error('Error fetching orders:', error);
+      }
+    };
+
+    fetchOpenOrders();
+  }, []); // Empty dependency array to run only once on mount
+
+  return (
+    <div>
+      <h2>Open Orders</h2>
+      {openOrders.map((order) => (
+        <OpenOrderCard key={order.id} order={order} />
+      ))}
+    </div>
+  );
+};
+
+export default ViewOrdersPage;

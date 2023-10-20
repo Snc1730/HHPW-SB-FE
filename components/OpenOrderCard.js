c141f027aece9662941ff0de1169aa11505cd977
Sean Bryant <smbryant1730@gmail.com>
2023-10-19 22:29:44 -0400

    finished delete order, can also close an order. Need to make closedOrder cards and functionality

--- components/OpenOrderCard.js
+++ components/OpenOrderCard.js
@@ -1,20 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
-const OpenOrderCard = ({ order }) => (
-  <div className="order-card">
-    <h3>Order Name: {order.orderName}</h3>
-    <p>Customer Name: {order.customerName}</p>
-    <p>Date Placed: {order.datePlaced}</p>
-    <Link href={`/order-details/${order.id}`} passHref>
-      <button type="button">View Details</button>
-    </Link>
-    <Link href={`/edit-order/${order.id}`} passHref>
-      <button type="button">Edit Order</button>
-    </Link>
-  </div>
-);
+const OpenOrderCard = ({ order, onDelete }) => {
+  const router = useRouter();
+
+  const handleClose = () => {
+    router.push(`/close-order/${order.id}`);
+  };
+
+  const handleDelete = () => {
+    const confirmed = window.confirm(`Are you sure you want to delete the order: ${order.orderName}?`);
+
+    if (confirmed) {
+      onDelete(order.id)
+        .then(() => {
+          window.location.reload();
+        })
+        .catch((error) => {
+          console.error('Error deleting order:', error);
+        });
+    }
+  };
+
+  return (
+    <div className="order-card">
+      <h3>Order Name: {order.orderName}</h3>
+      <p>Customer Name: {order.customerName}</p>
+      <p>Date Placed: {order.datePlaced}</p>
+      <Link href={`/order-details/${order.id}`} passHref>
+        <button type="button">View Details</button>
+      </Link>
+      <Link href={`/edit-order/${order.id}`} passHref>
+        <button type="button">Edit Order</button>
+      </Link>
+      <button type="button" onClick={handleClose}>
+        Close Order
+      </button>
+      <button type="button" onClick={handleDelete}>
+        Delete Order
+      </button>
+    </div>
+  );
+};
 
 OpenOrderCard.propTypes = {
   order: PropTypes.shape({
@@ -22,8 +51,8 @@ OpenOrderCard.propTypes = {
     orderName: PropTypes.string.isRequired,
     customerName: PropTypes.string.isRequired,
     datePlaced: PropTypes.string.isRequired,
-    // Add other properties from your Order model as needed
   }).isRequired,
+  onDelete: PropTypes.func.isRequired,
 };
 
 export default OpenOrderCard;
---
+++ pages/close-order/[id].js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { getOrderById } from '../../api/OrderEndpoints';
+import CloseOrderForm from '../../components/CloseOrderForm';
+
+const CloseOrderPage = () => {
+  const router = useRouter();
+  const { id } = router.query;
+  const [orderData, setOrderData] = useState(null);
+
+  useEffect(() => {
+    const fetchOrderData = async () => {
+      try {
+        const order = await getOrderById(id); // Fetch order data
+        setOrderData(order);
+      } catch (error) {
+        // Handle errors
+        console.error(error.message);
+      }
+    };
+
+    fetchOrderData();
+  }, [id]);
+
+  return (
+    <div>
+      <h2>Close Order</h2>
+      {orderData && (
+        <div>
+          <h3>Order Name: {orderData.orderName}</h3>
+          <p>Order Price: {orderData.orderPrice}</p>
+          <CloseOrderForm order={orderData} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default CloseOrderPage;
